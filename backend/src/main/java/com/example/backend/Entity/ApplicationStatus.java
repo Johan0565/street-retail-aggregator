466a7d9e2090837eea6a916f905b34fb5cd5ecb7
Магомед <magomedxaxa_000@mail.ru>
2026-03-16 19:56:58 +0300

    Added enums

---
+++ backend/src/main/java/com/example/backend/Entity/ApplicationStatus.java
@@ -0,0 +1,5 @@
+package com.example.backend.Entity;
+
+public enum ApplicationStatus {
+    PENDING, REVIEWING, ACCEPTED, REJECTED
+}
---
+++ backend/src/main/java/com/example/backend/Entity/PropertyStatus.java
@@ -0,0 +1,5 @@
+package com.example.backend.Entity;
+
+public enum PropertyStatus {
+    DRAFT, PUBLISHED, RENTED, ARCHIVED
+}
