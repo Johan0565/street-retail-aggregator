9e406120a057f2878a4d3e2043a8de40b8c9f445
Магомед <magomedxaxa_000@mail.ru>
2026-03-19 23:40:04 +0300

    Ykit integration

--- frontend/android/app/src/main/java/com/example/frontend/MainActivity.java
+++ frontend/android/app/src/main/java/com/example/frontend/MainActivity.java
@@ -1,6 +1,15 @@
-package com.example.frontend;
+package com.example.your_project_name; // Не забудь оставить свой пакет!
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import com.yandex.mapkit.MapKitFactory;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        // API-ключ от Яндекс.Карт
+        MapKitFactory.setApiKey("1cdf3269-ddc3-4fc5-9f3b-40550c256370");
+        super.configureFlutterEngine(flutterEngine);
+    }
 }
